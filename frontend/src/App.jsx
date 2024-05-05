54af08589dfb333bcbacb04c967dee18624a813f
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2024-05-05 16:15:53 +0530

    Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 //Pages
+import RootLayout from "./pages/Root";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
 import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 
+//Routes
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/events", element: <Events /> },
-  { path: "/events/:eventId", element: <EventDetail /> },
-  { path: "/events/new", element: <NewEvent /> },
-  { path: "/events/:eventId/edit", element: <EditEvent /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "events", element: <Events /> },
+      { path: "events/:eventId", element: <EventDetail /> },
+      { path: "events/new", element: <NewEvent /> },
+      { path: "events/:eventId/edit", element: <EditEvent /> },
+    ],
+  },
 ]);
 
 function App() {
